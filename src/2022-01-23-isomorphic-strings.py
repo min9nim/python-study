2bbf3883dbe8ab34300ae608e972b58954057b0d
Keating <min9nim@gmail.com>
2022-01-23 19:41:12 +0900

    more solution

--- src/2022-01-23-isomorphic-strings.py
+++ src/2022-01-23-isomorphic-strings.py
@@ -28,4 +28,45 @@ assert Solution().isIsomorphic('badc', 'baba') == False
 assert Solution().isIsomorphic("bbbaaaba", "aaabbbba") == False
 assert Solution().isIsomorphic("egcd", "adfd") == False
 
+
+class Solution2:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        stMap = {}
+        tsMap = {}
+
+        for a, b in zip(s, t):
+            if a not in stMap:
+                stMap[a] = b
+            if b not in tsMap:
+                tsMap[b] = a
+
+            if not (stMap[a] == b and tsMap[b] == a):
+                return False
+
+        return True
+
+
+assert Solution2().isIsomorphic('a', 'b') == True
+assert Solution2().isIsomorphic('add', 'egg') == True
+assert Solution2().isIsomorphic('foo', 'bar') == False
+assert Solution2().isIsomorphic('paper', 'title') == True
+assert Solution2().isIsomorphic('badc', 'baba') == False
+assert Solution2().isIsomorphic("bbbaaaba", "aaabbbba") == False
+assert Solution2().isIsomorphic("egcd", "adfd") == False
+
+
+class Solution3:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t))
+
+
+assert Solution3().isIsomorphic('a', 'b') == True
+assert Solution3().isIsomorphic('add', 'egg') == True
+assert Solution3().isIsomorphic('foo', 'bar') == False
+assert Solution3().isIsomorphic('paper', 'title') == True
+assert Solution3().isIsomorphic('badc', 'baba') == False
+assert Solution3().isIsomorphic("bbbaaaba", "aaabbbba") == False
+assert Solution3().isIsomorphic("egcd", "adfd") == False
+
+
 print('success!')
