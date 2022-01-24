43800ce39cd6dfea59a9cc60e17e3cb6746dfb5c
Keating <min9nim@gmail.com>
2022-01-24 21:18:39 +0900

    new

---
+++ src/2022-01-24-long-pressed-name.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/long-pressed-name/
+
+
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        repeatedStr = ''
+        size = len(name)
+        for i, v in enumerate(name):
+            repeatedStr += v
+            if i < size-1 and name[i+1] == v:
+                continue
+            else:
+                if repeatedStr == typed[0:len(repeatedStr)]:
+                    typed = typed[len(repeatedStr):]
+                    while typed != '' and typed[0] == v:
+                        typed = typed[1:]
+                    repeatedStr = ''
+                else:
+                    return False
+        return typed == ''
+
+
+assert Solution().isLongPressedName('alex', 'aaleex') == True
+assert Solution().isLongPressedName('saeed', 'ssaaedd') == False
+
+
+print('success!')
