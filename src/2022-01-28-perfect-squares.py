0737ad475bfcf069ab65828fee34fded841fc299
Keating <min9nim@gmail.com>
2022-01-28 23:51:27 +0900

    new

---
+++ src/2022-01-28-perfect-squares.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/perfect-squares/
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        return 3
+
+
+assert Solution().numSquares(12) == 3
+
+
+print('success!')
