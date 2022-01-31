4e5b73232632761cf4c589a1ad4bf4c85d8010ea
Keating <min9nim@gmail.com>
2022-01-31 09:27:50 +0900

    new

---
+++ src/2022-01-31-ugly-number.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n < 1:
+            return False
+        while True:
+            if n % 2 == 0:
+                n = n // 2
+            elif n % 3 == 0:
+                n = n // 3
+            elif n % 5 == 0:
+                n = n // 5
+            else:
+                return n == 1
+
+
+assert Solution().isUgly(6) == True
+assert Solution().isUgly(1) == True
+assert Solution().isUgly(14) == False
+assert Solution().isUgly(0) == False
+
+
+print('done')
