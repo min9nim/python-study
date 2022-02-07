a21faa50837851a61107674b486798f8940b1583
Keating <min9nim@gmail.com>
2022-02-08 04:52:14 +0900

    new solution

--- src/2022-01-24-long-pressed-name.py
+++ src/2022-01-24-long-pressed-name.py
@@ -24,4 +24,19 @@ assert Solution().isLongPressedName('alex', 'aaleex') == True
 assert Solution().isLongPressedName('saeed', 'ssaaedd') == False
 
 
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i = 0
+        for j in range(len(typed)):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+            elif i == 0 or name[i - 1] != typed[j]:
+                return False
+        return i == len(name)
+
+
+assert Solution().isLongPressedName('alex', 'aaleex') == True
+assert Solution().isLongPressedName('saeed', 'ssaaedd') == False
+
+
 print('success!')
