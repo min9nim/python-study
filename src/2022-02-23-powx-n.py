05d1b4f0c441cbeb0852ed1a0b202da1d73d8aad
Keating <min9nim@gmail.com>
2022-02-23 07:33:42 +0900

    new solution

--- src/2022-02-23-powx-n.py
+++ src/2022-02-23-powx-n.py
@@ -23,4 +23,35 @@ class Solution:
 assert Solution().myPow(2.00000, 10) == 1024.00000
 
 
+class Solution:
+    def __init__(self):
+        self.map = dict()
+
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n in self.map:
+            return self.map[n]
+
+        isPositive = n > 0
+        if not isPositive:
+            n = -n
+
+        n1 = n // 2
+        n2 = n - n1
+
+        result = self.myPow(x, n1) * self.myPow(x, n2)
+
+        if isPositive:
+            self.map[n] = result
+            return result
+        else:
+            return 1 / result
+
+
+assert Solution().myPow(2.00000, 10) == 1024.00000
+assert Solution().myPow(2.10000, 3) == 9.261000000000001
+
 print('done')
