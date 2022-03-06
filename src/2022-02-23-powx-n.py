876703e691aa5c2182e56afaa6e0deb0be8a4e4c
Keating <min9nim@gmail.com>
2022-03-07 07:33:26 +0900

    new solution

--- src/2022-02-23-powx-n.py
+++ src/2022-02-23-powx-n.py
@@ -53,5 +53,30 @@ class Solution:
 
 assert Solution().myPow(2.00000, 10) == 1024.00000
 assert Solution().myPow(2.10000, 3) == 9.261000000000001
+assert Solution().myPow(2.00000, -2) == 0.250000
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1/x
+            n = abs(n)
+
+        tmp = x
+        res = 1
+        while n:
+            # print(n, tmp, res)
+            if n % 2 == 1:
+                res *= tmp
+            tmp *= tmp
+            n //= 2
+
+        return res
+
+
+assert Solution().myPow(2.00000, 10) == 1024.00000
+assert Solution().myPow(2.10000, 3) == 9.261000000000001
+assert Solution().myPow(2.00000, -2) == 0.250000
+
 
 print('done')
