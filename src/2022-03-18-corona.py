68e97e5a492d07b07c876f3ee5a57b2cb6de7c1a
keating <min9nim@gmail.com>
2022-03-21 07:31:19 +0900

    fix bug

--- src/2022-03-18-corona.py
+++ src/2022-03-18-corona.py
@@ -1,37 +1,31 @@
 def solution(seat):
     le = len(seat)
-    result = [[0]*le]*le
+    result = [[0]*le for _ in range(le)]
 
-    def mark(x, y, noMask):
-        if x >= 0 and x < le and y >= 0 and y < le:
-            c = seat[x][y]
-            if c == '-':
-                return
-            if noMask and c == 'N':
-                result[x][y] = 1
-            else:
-                result[x][y] = 1
-
-    def traverse(x, y, noMask, d):
-        mark(x-1, y, noMask)
-        visit(x-1, y, d+1)
-        mark(x+1, y, noMask)
-        visit(x+1, y, d+1)
-        mark(x, y-1, noMask)
-        visit(x, y-1, d+1)
-        mark(x, y-1, noMask)
-        visit(x, y-1, d+1)
+    for arr in result:
+        print(arr)
 
     def visit(x, y, d):
-        if d > 3:
-            return
-        traverse(x, y, True if d == 3 else False, d)
+        if 0 <= x < le and 0 <= y < le:
+            c = seat[x][y]
+            if c != '-' and c != 'C':
+                if d < 3 or (d == 3 and c == 'N'):
+                    result[x][y] = 1
+
+            if d < 3:
+                visit(x-1, y, d+1)
+                visit(x+1, y, d+1)
+                visit(x, y-1, d+1)
+                visit(x, y+1, d+1)
 
     for i in range(le):
         for j in range(le):
             c = seat[i][j]
             if c == 'C':
-                visit(i, j, 1)
+                visit(i-1, j, 1)
+                visit(i+1, j, 1)
+                visit(i, j-1, 1)
+                visit(i, j+1, 1)
 
     for str in seat:
         print(list(str))
@@ -44,3 +38,6 @@ def solution(seat):
 assert solution(
     ["M--NN-", "-M----", "-CC--M", "-N----", "N-M-C-", "-M----"]
 ) == 5
+
+
+print('done')
